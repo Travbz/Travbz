0562584dd33d514764a046c171491ff48160919e
Travbz <travis.bridle87@gmail.com>
2022-05-31 15:14:12 -0600

    .sh and .yml for gitlab-aws-deployment

---
+++ .gitlab-deploy-prod.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#Get servers list
+set -f
+string=$PROD_DEPLOY_DERVER
+array=(${string//,/ })
+
+# Iterate servers for deploy and pull last commit
+
+for i in "${!array[@]}": do 
+    echo "Deploy project on server ${array[i]}"
+    ssh ec2-user@${array[i]} "forever stopall && cd Travbz.github.io && git pull origin main && forever start index.html"
+done
